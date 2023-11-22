b7a3b8c1ee810dc39ddcf4946298b4647b84a7a4
N100rot <nickhaha245@gmail.com>
2023-11-22 22:05:42 +0530

    added floating cursor and tilt.js

--- script.js
+++ script.js
@@ -2,19 +2,42 @@ const container = document.querySelector("#imgContainer");
 const img = document.querySelector("#imgContainer img")
 const cursor = document.querySelector("#crsr");
 
+document.addEventListener("mousemove", (dets) => {
 
-container.addEventListener("mousemove", (dets) => {
-    const x = dets.x / 20;
-    const y = dets.y / 30;
-    console.log(x);
-    console.log(y);
-
-    img.style.transform = `rotateX(${x}deg) rotateY(${-y}deg)`;
+    cursor.style.left = dets.x + "px";
+    cursor.style.top = dets.y + "px";
+    cursor.style.transition = "linear .15s"
+})
+img.addEventListener("mouseover", () => {
+    cursor.style.opacity = "1";
+})
+img.addEventListener("mouseleave", () => {
+    cursor.style.opacity = "0";
 })
 
-container.addEventListener("mouseleave", (dets) => {
-    setTimeout(() => {
-        img.style.transform = `rotateX(0deg) rotateY(0deg)`
-    }, 1000);
- 
+VanillaTilt.init(document.querySelector("#imgContainer img"), {
+    reverse: false,
+    perspective: 1000,
+    transition: "ease 1s",
+    scale: 1.01
 });
+
+
+
+
+
+// container.addEventListener("mousemove", (dets) => {
+//     const x = dets.x / 20;
+//     const y = dets.y / 30;
+//     console.log(x);
+//     console.log(y);
+
+//     img.style.transform = `rotateX(${x}deg) rotateY(${-y}deg)`;
+// })
+
+// container.addEventListener("mouseleave", (dets) => {
+//     setTimeout(() => {
+//         img.style.transform = `rotateX(0deg) rotateY(0deg)`
+//     }, 1000);
+ 
+// });
